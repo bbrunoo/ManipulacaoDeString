24deb4f8c3c3239e92e1a20bf599680381508a13
bbrunoo <brunoedsanntos@gmail.com>
2021-12-13 23:54:31 -0300

    mstirng

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace ManipStirng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Write("Escreva uma Frase/Palavra : ");
+            string nome = ReadLine().ToLower();
+            Write("Digite a letra que deseja trocar: ");
+            string letra = ReadLine().ToLower();
+            Write("Digite a letra que deseja colocar no lugar: ");
+            string NovaLetra = ReadLine().ToLower();
+            WriteLine("--------------------------------------------");
+            
+            if(nome.Contains(letra))
+            {
+               WriteLine($"Frase/Palavra  antes de trocar a letra: {nome}");  
+               nome = nome.Replace(letra, NovaLetra);
+               WriteLine($"Frase/Palavra depois de trocar a letra: {nome}");
+            }
+            
+            else
+            {
+                    WriteLine("Não há nenhuma letra nessa frase");
+            }
+            WriteLine("--------------------------------------------");
+
+        }
+    }
+}
